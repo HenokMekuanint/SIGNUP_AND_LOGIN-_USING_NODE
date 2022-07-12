b0549be59faeda5dc4811379fd5af0f6cb7782f6
unknown <henokmekuanint79@gmail.com>
2022-07-12 19:22:18 +0300

    passpportadded

--- server.js
+++ server.js
@@ -1,14 +1,34 @@
+if(process.env.NODE_ENV!=='production'){
+    require('dotenv').config()
+}
 const express=require('express')
 const app=express()
 const passport = require('passport')
-
 const bcrypt=require('bcrypt')
 const initializepassport=require("./passport-config")
+const flash = require('express-flash')
+const session = require('express-session')
+
+initializepassport(
+
+    passport,
+    email=>{
+        return users.find(user=>user.email===email)
+    }
+)
 
 initializepassport(passport)
 const users=[]
 app.set('view-engine','ejs')
 app.use(express.urlencoded({extended:false}))
+app.use(flash)
+app.use(session({
+    secret:process.env.SESSION_SECRET,
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.get('/',(req,res)=>{
 res.render('index.ejs',{name:" hena"})
 }
--- views/passport-config.js
+++ views/passport-config.js
@@ -1,13 +1,23 @@
 const { authenticate } = require('passport')
-
+const bcrypt=require('bcrypt')
 const LocalStrategy=require('passport-local').Strategy
-function initialize(passport){
+function initialize(passport,getUerByEmail){
     const authenticateUser=(email,password,done)=>{
         const user=getUerByEmail(email)
         if(user==email){
-            return done(null)
+            return done(null,false,{message:'No User with that email'})
         }
-
+try {
+    if(await bcrypt.compare(password,user.password))
+    {
+        return done(null,user)
+    }
+    else{
+        return done(null,false,{message:"password incorrect"})
+    }
+} catch {
+    
+}
     }
  passport.use(new LocaleStrategy({usernameFiled:'email'}),
  authenticateUser)
