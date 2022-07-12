f058166375488d52cb23ee72f1ff1bb9d8c6dfaf
unknown <henokmekuanint79@gmail.com>
2022-07-12 16:57:14 +0300

    signupadded

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express=require('express')
 const app=express()
+const bcrypt=require('bcrypt')
+
+const users=[]
 app.set('view-engine','ejs')
+app.use(express.urlencoded({extended:false}))
 app.get('/',(req,res)=>{
 res.render('index.ejs',{name:" hena"})
 }
@@ -15,4 +19,25 @@ app.get('/register',(req,res)=>{
     res.render('register.ejs')
     }
     )
+
+app.post("/register",async(req,res)=>{
+    try {
+        const hashedpassword=await bcrypt.hash(req.body.password,10)
+        users.push({
+            id:Date.now().toString(),
+            name:req.body.name,
+            email:req.body.email,
+            password:hashedpassword
+
+
+        })
+        res.redirect("/login")
+    } catch  {
+        res.redirect("register")
+    }
+console.log(users)
+})
+app.post("/login",(req,res)=>{
+    
+})
 app.listen(3000)
